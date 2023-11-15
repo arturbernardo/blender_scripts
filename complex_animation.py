e7d9dd5129d2bfd7e1a7da6086a038192fa81a83
arturbernardo <artur.bernardo.k@gmail.com>
2023-11-15 20:24:25 -0300

    Update complex_animation.py
    Up, spin and run to horizon.

--- complex_animation.py
+++ complex_animation.py
@@ -1,6 +1,8 @@
 import bpy
 import math
 
+#Cria objetos e os faz rodar e subir
+
 #A
 bpy.ops.mesh.primitive_cube_add(align='WORLD', location=(0, -6, 0), scale=(1, 1, 1))
 a_door = bpy.context.object
@@ -45,3 +47,35 @@ c_door.location.z = 0
 c_door.keyframe_insert(data_path = "location", frame = 80)
 c_door.rotation_euler.z = math.radians(360) #
 c_door.keyframe_insert(data_path = "rotation_euler", frame = 80) #
+
+
+# Os dispara para o infinito
+#A
+a_door.location.x = -200
+a_door.keyframe_insert(data_path = "location", frame = 100)
+
+a_door.scale = (1,1,1)
+a_door.keyframe_insert(data_path = "scale", frame = 100)
+
+a_door.scale = (0,0,0)
+a_door.keyframe_insert(data_path = "scale", frame = 110)
+
+#B
+b_door.location.x = -200
+b_door.keyframe_insert(data_path = "location", frame = 100)
+
+b_door.scale = (1,1,1)
+b_door.keyframe_insert(data_path = "scale", frame = 100)
+
+b_door.scale = (0,0,0)
+b_door.keyframe_insert(data_path = "scale", frame = 110)
+
+#C
+c_door.location.x = -200
+c_door.keyframe_insert(data_path = "location", frame = 100)
+
+c_door.scale = (1,1,1)
+c_door.keyframe_insert(data_path = "scale", frame = 100)
+
+c_door.scale = (0,0,0)
+c_door.keyframe_insert(data_path = "scale", frame = 110)
