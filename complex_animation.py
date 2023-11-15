b1c0299e3483acd0d490a30fc12538ff56bffd10
arturbernardo <artur.bernardo.k@gmail.com>
2023-11-15 19:14:56 -0300

    Create complex_animation.py
    INIT

---
+++ complex_animation.py
@@ -0,0 +1,31 @@
+import bpy
+
+#A
+bpy.ops.mesh.primitive_cube_add(align='WORLD', location=(0, -6, 0), scale=(1, 1, 1))
+a_door = bpy.context.object
+a_door.name = "A"
+
+a_door.location.z = -10
+a_door.keyframe_insert(data_path = "location", frame = 1)
+a_door.location.z = 0
+a_door.keyframe_insert(data_path = "location", frame = 80)
+
+#B
+bpy.ops.mesh.primitive_cube_add(align='WORLD', location=(0, 0, 0), scale=(1, 1, 1))
+b_door = bpy.context.object
+b_door.name = "B"
+
+b_door.location.z = -10
+b_door.keyframe_insert(data_path = "location", frame = 1)
+b_door.location.z = 0
+b_door.keyframe_insert(data_path = "location", frame = 80)
+
+#C
+bpy.ops.mesh.primitive_cube_add(align='WORLD', location=(0, 6, 0), scale=(1, 1, 1))
+c_door = bpy.context.object
+c_door.name = "C"
+
+c_door.location.z = -10
+c_door.keyframe_insert(data_path = "location", frame = 1)
+c_door.location.z = 0
+c_door.keyframe_insert(data_path = "location", frame = 80)
