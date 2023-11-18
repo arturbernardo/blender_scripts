696a9dd09f044452823741977bafc95fe1cd470f
arturbernardo <artur.bernardo.k@gmail.com>
2023-11-18 16:02:26 -0300

    Update monty_hall_project.py

--- monty_hall_project.py
+++ monty_hall_project.py
@@ -8,8 +8,55 @@ camera = bpy.context.object
 camera.location = [22, 0, 13]
 camera.rotation_euler = [math.radians(63), 0, math.radians(90)]
 
-counter = 0
+#banner
+bpy.ops.mesh.primitive_cube_add(align='WORLD', rotation=(math.radians(90), 0, math.radians(90)), location=(0, -3, 4), scale=(2, 2, 2))
+banner = bpy.context.object
+banner.name = 'banner'
 
+#Material
+material = bpy.data.materials.new(name="bannerMaterial")
+material.diffuse_color = (1.0, 0.0, 1.0, 1.0)
+
+#create modifier and nodes
+bpy.ops.node.new_geometry_nodes_modifier()
+modifier = bpy.context.object.modifiers['GeometryNodes']
+node_group = modifier.node_group
+node_string_to_curve = node_group.nodes.new('GeometryNodeStringToCurves')
+node_value_to_string= node_group.nodes.new('FunctionNodeValueToString')
+
+node_resample = node_group.nodes.new('GeometryNodeResampleCurve')
+node_resample.mode = 'LENGTH'
+node_resample.inputs[3].default_value = 0.01
+
+node_fill = node_group.nodes.new('GeometryNodeFillCurve')
+# Better topology
+node_fill.mode = 'NGONS' #Triangles is default
+
+node_value = node_group.nodes.new('ShaderNodeValue')
+
+node_material = node_group.nodes.new('GeometryNodeSetMaterial')
+node_material.inputs[2].default_value = bpy.data.materials["bannerMaterial"]
+
+
+#probably exists
+node_output = node_group.nodes.get("Group Output")
+if (node_output == None):
+    node_output = node_group.nodes.new('NodeGroupOutput')
+
+#Remove default input if exists
+to_be_removed = node_group.nodes.get("Group Input")
+if (to_be_removed != None):
+    node_group.nodes.remove(to_be_removed)
+
+# Conect output to input
+node_group.links.new(node_value.outputs[0], node_value_to_string.inputs[0])
+node_group.links.new(node_value_to_string.outputs[0], node_string_to_curve.inputs[0])
+node_group.links.new(node_string_to_curve.outputs[0], node_resample.inputs[0])
+node_group.links.new(node_resample.outputs[0], node_fill.inputs[0])
+node_group.links.new(node_fill.outputs[0], node_material.inputs[0])
+node_group.links.new(node_material.outputs[0], node_output.inputs[0])
+
+#Create doors
 #A
 bpy.ops.mesh.primitive_cube_add(align='WORLD', location=(0, -6, 0), scale=(1, 1, 1))
 a_door = bpy.context.object
@@ -25,6 +72,8 @@ bpy.ops.mesh.primitive_cube_add(align='WORLD', location=(0, 6, 0), scale=(1, 1, 
 c_door = bpy.context.object
 c_door.name = "C"
 
+# Monty Hall animation
+counter = 0
 starting_frame = 62
 step_frame = 12
 double_step_frame = 24
@@ -65,39 +114,6 @@ if (porta_revelada == 2):
     c_door.keyframe_insert(data_path = "scale", frame = starting_frame + step_frame)
     c_door.scale = [0,0,0]
     c_door.keyframe_insert(data_path = "scale", frame = starting_frame + double_step_frame)
-
-#banner
-bpy.ops.object.text_add(align='WORLD', rotation=(math.radians(90), 0, math.radians(90)), location=(0, -3, 4), scale=(2, 2, 2))
-banner = bpy.context.object
-banner.name = 'banner'
-banner.data.body = "0"
-
-material = bpy.data.materials.new(name="MeuMaterial")
-material.diffuse_color = (1.0, 0.0, 1.0, 1.0)
-banner.data.materials.append(material)
-
-#create modifier and nodes
-bpy.ops.node.new_geometry_nodes_modifier()
-modifier = bpy.context.object.modifiers['GeometryNodes']
-node_group = modifier.node_group
-node_string_to_curve = node_group.nodes.new('GeometryNodeStringToCurves')
-node_value_to_string= node_group.nodes.new('FunctionNodeValueToString')
-node_value = node_group.nodes.new('ShaderNodeValue')
-#probably exists
-node_output = node_group.nodes.get("Group Output")
-if (node_output == None):
-    node_output = node_group.nodes.new('NodeGroupOutput')
-
-#Remove default input
-to_be_removed = node_group.nodes.get("Group Input")
-if (to_be_removed != None):
-    node_group.nodes.remove(to_be_removed)
-
-# Conect output to input
-node_group.links.new(node_value.outputs[0], node_value_to_string.inputs[0])
-node_group.links.new(node_value_to_string.outputs[0], node_string_to_curve.inputs[0])
-node_group.links.new(node_string_to_curve.outputs[0], node_output.inputs[0])
-
 
 if (escolha_jogador == porta_premiada):
     frame_atual = bpy.context.scene.frame_current
